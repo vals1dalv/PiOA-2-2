94ff8543b2e13ea7f2c328744649426ac4ff0e13
Владислав <nekijvladislav@gmail.com>
2025-06-08 02:45:30 +0300

    Add files via upload

--- Задача 8/Ex8/ConsoleAppEx8/Program.cs
+++ Задача 8/Ex8/ConsoleAppEx8/Program.cs
@@ -10,37 +10,26 @@ namespace lab_8
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Clear();
 
-            int n; // Кол-во элементов ряда
-            double result = 0;
-
-            // Ввод с проверкой
-            for (; ; )
-            {
-                Console.Write("Введите кол-во элементов ряда (n>= 4): ");
-                if (int.TryParse(Console.ReadLine(), out n) && n >= 4)
-                {
-                    break;
-                }
-                Console.WriteLine("Ошибка: введите целое число больше или равное 4");
-            }
+            double sum;
+            double result;
 
             // Формализация ряда в цикле
-            double numerator = 0;
-            double denominator = 0;
+            result = 0;
 
-            for (int k = 1; k <= n; k++)
+            for (int k = 1; k <= 10; k++)
             {
-                // Вычисление элемента ряда
-                double term = Math.Tan(2 * k);
-                numerator += term; // Добавл. элемента к числит.
-                denominator += k; // Добавл. элемента к знаменат.
+                sum = 0;
+
+                for (int n = 1; n <= k; n++)
+                {
+                    sum += Math.Tan(k * n); // Сумма tg(k*m)
+                }
+
+                result += sum / k;
             }
 
-            // Вычисление результата
-            result = numerator / denominator;
-
             // Вывод результата
-            Console.WriteLine($"Сумма первых {n} элементов ряда: {result:F6}");
+            Console.WriteLine($"Результат вычисления ряда: {result:F6}");
 
             Console.WriteLine("Нажмите любую клавишу для завершения");
             Console.ReadKey(true);
